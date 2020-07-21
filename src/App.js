897eefa823cc68c7a92bd5182615cc0875eb8c57
bashir2017 <bashiralhanshali@yahoo.com>
2020-07-21 19:08:33 -0400

    final

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import MouseComponent from './MouseComponent';
 
 class App extends Component {
 	render() {
-		// your code in the return statement below!
 		return (
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
 				<MouseComponent />
 			</div>
 		);
